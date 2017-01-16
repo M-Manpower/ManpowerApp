04356e17b3343e6b93cc874a4d5dfa7bf4fcddb6
Jonna Aguisanda <JonnaA@Magenic.com>
2017-01-16 22:42:40 +0800

    created project

---
+++ Magenic.Manpower/js/config.route.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('manpowerApp')
+    .run(['routehelper', function (routehelper) {
+        routehelper.configureRoutes(getRoutes());
+    }]);
+
+    function getRoutes() {
+        return [{
+            url: '/dashboard',
+            config: {
+                templateUrl: '',
+                controller: '',
+                controllerAs: 'vm',
+                title: ''
+            }
+        }]
+    };
+})();
