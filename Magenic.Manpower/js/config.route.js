4e1461f010afbe009c6910fa944274b0f8dc14a6
Jonna Aguisanda <JonnaA@Magenic.com>
2017-01-17 20:34:37 +0800

    restructure of folder

--- Magenic.Manpower/js/config.route.js
+++ Magenic.Manpower/js/config.route.js
@@ -3,13 +3,17 @@
 
     angular.module('manpowerApp', ['ui.router'])
     .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
-        $urlRouterProvider.otherwise('/login');
+        $urlRouterProvider.otherwise('/','/login');
 
         $stateProvider
+            .state('/',
+            {
+                url: '/'
+            })
             .state('login',
             {
                 url: '/login',
-                templateUrl: '/templates/login.html',
+                templateUrl: '/modules/login/login.html',
                 controller: 'LoginController',
                 controllerAs: 'vm'
             })
