29307875d5dc695ef88b937ac1467b85fcf726ff
jezmaghuyop <JezM@magenic.com>
2017-01-19 00:08:27 +0800

    update skills list view

--- Magenic.Manpower/modules/primarySkills/primarySkillsList.component.js
+++ Magenic.Manpower/modules/primarySkills/primarySkillsList.component.js
@@ -5,16 +5,18 @@
     const primarySkillListComponent = {
         bindings: {},
         templateUrl: 'modules/primarySkills/primarySkillsList-tpl.html',
-        controller: function () {
+        controller: ['_', function (_) {
+
+            this.isAllSkillsSelected = false;
 
             this.$onInit = function () {
             
                 this.primarySkills = [
-                    { id: 1, name: 'Java', description: 'This is a PL 1', active: false },
-                    { id: 2, name: '.Net', description: 'This is a PL 2', active: false },
-                    { id: 3, name: 'PHP', description: 'This is a PL 3', active: false },
-                    { id: 4, name: 'C#', description: 'This is a PL 4', active: false },
-                    { id: 5, name: 'VB.NET', description: 'This is a PL 5', active: false }
+                    { id: 1, name: 'Java', description: 'This is a PL 1', active: true, selected: false },
+                    { id: 2, name: '.Net', description: 'This is a PL 2', active: false, selected: false },
+                    { id: 3, name: 'PHP', description: 'This is a PL 3', active: true, selected: false },
+                    { id: 4, name: 'C#', description: 'This is a PL 4', active: false, selected: false },
+                    { id: 5, name: 'VB.NET', description: 'This is a PL 5', active: true, selected: false }
                 ];
                 
             };
@@ -23,19 +25,29 @@
                
             };
 
-            this.selectedSkill = function (id) {
+            this.selectAllSkills = function () {
 
-                console.log(id);
+                _.each(this.primarySkills, (skill) => {
+
+                    skill.selected = this.isAllSkillsSelected;
+
+                });
 
             };
 
-            this.deactivate = function () {
+            this.deactivateSelectedSkills = function () {                
 
+                _.each(this.primarySkills, (skill) => {
+                    
+                    if (skill.selected) {
+                        skill.active = false;
+                    }
 
+                });
 
             };
             
-        }
+        }]
     };
 
 
