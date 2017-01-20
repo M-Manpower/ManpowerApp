92a42b5e878cc54c15037672f261f50bed9c6dcb
RolandJ <RolandJ@ROLANDJ-PC1>
2017-01-20 15:12:10 +0800

    WebAPI|RolandJ|Added swagger support

--- Magenic.Manpower.WebApi/Startup.cs
+++ Magenic.Manpower.WebApi/Startup.cs
@@ -38,6 +38,8 @@ namespace Magenic.Manpower.WebApi
 
             services.AddDbContext<MagenicManpowerDBContext>(options =>
              options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +49,8 @@ namespace Magenic.Manpower.WebApi
             loggerFactory.AddDebug();
             app.UseCors("CorsPolicy");
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUi();
         }
     }
 }
